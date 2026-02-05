1d1a4c86dd5459c977e159a74753fb5ebdd62756
fookang <fookang@gmail.com>
2026-02-05 18:23:44 +0800

    Complete code for Lab2_3

--- lab_2/as02_3.c
+++ lab_2/as02_3.c
@@ -20,12 +20,17 @@
  * @brief   Application entry point.
  */
 
-// LED pin numbers
+// LED pin numbers22
 #define RED_PIN		31	// PTE31
 #define GREEN_PIN	5	// PTD5
 #define BLUE_PIN	29	// PTE29
 
 #define SWITCH_PIN   4  // PTA4
+
+#define ADC_PIN1 30
+#define ADC_PIN1_CHANNEL 23
+#define ADC_PIN2 2
+#define ADC_PIN2_CHANNEL 12
 
 typedef enum tl {
 	RED, GREEN, BLUE
@@ -43,47 +48,187 @@ typedef enum tl {
 
 //TODO
 void initADC() {
+	// Disable & clear interrupt
+	NVIC_DisableIRQ(ADC0_IRQn);
+	NVIC_ClearPendingIRQ(ADC0_IRQn);
+
+	// Enable clock gating to relevant configurations
+	SIM->SCGC6 |= SIM_SCGC6_ADC0_MASK;
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
+
+	// Set pins from Q3 to ADC
+	PORTE->PCR[ADC_PIN1] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[ADC_PIN1] |= PORT_PCR_MUX(0b0);
+
+	PORTB->PCR[ADC_PIN2] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[ADC_PIN2] |= PORT_PCR_MUX(0b0);
+
+	// Configure the ADC
+	// Enable ADC interrupt
+	ADC0->SC1[0] |= ADC_SC1_AIEN_MASK;
+
+	// Select single-ended ADC
+	ADC0->SC1[0] &= ~ADC_SC1_DIFF_MASK;
+	ADC0->SC1[0] |= ADC_SC1_DIFF(0b0);
+
+	// Set 12 bit conversion
+	ADC0->CFG1 &= ~ADC_CFG1_MODE_MASK;
+	ADC0->CFG1 |= ADC_CFG1_MODE(0b01);
+
+	// Select software conversion trigger
+	ADC0->SC2 &= ~ADC_SC2_ADTRG_MASK;
+
+	// Configure alternate voltage reference
+	ADC0->SC2 &= ~ADC_SC2_REFSEL_MASK;
+	ADC0->SC2 |= ADC_SC2_REFSEL(0b01);
+
+	// Don't use averaging
+	ADC0->SC3 &= ~ADC_SC3_AVGE_MASK;
+	ADC0->SC3 |= ADC_SC3_AVGE(0);
+
+	// Switch off continuous conversion.
+	ADC0->SC3 &= ~ADC_SC3_ADCO_MASK;
+	ADC0->SC3 |= ADC_SC3_ADCO(0);
+
+	// Set highest priority
+	NVIC_SetPriority(ADC0_IRQn, 0);
+	NVIC_EnableIRQ(ADC0_IRQn);
 }
 
 int result[2];
 
 void startADC(int channel) {
+	//mask and set the channel
+	ADC0->SC1[0] &= ~ADC_SC1_ADCH_MASK;
+	ADC0->SC1[0] |= ADC_SC1_ADCH(channel);
 }
 
 //TODO! This is NOT going to work.
-void ADCX_IRQHandler(){
+void ADC0_IRQHandler(){
 	static int turn=0;
 
-	NVIC_ClearPendingIRQ(ADCX_IRQn);
-	if(ADCX->SC1[0] & ADC_SC1_COCO_MASK) {
-		result[turn] = ADCX->R[0];
+	NVIC_ClearPendingIRQ(ADC0_IRQn);
+
+	if(ADC0->SC1[0] & ADC_SC1_COCO_MASK) {
+		result[turn] = ADC0->R[0];
 		//PRINTF("Turn = %d, Result = %d\r\n", turn, result[turn]);
 		turn = 1 - turn;
 		if(turn == 0) {
-			startADC(ADC_XXX);
+			startADC(ADC_PIN1_CHANNEL);
 		} else {
-			startADC(ADC_YYY);
+			startADC(ADC_PIN2_CHANNEL);
 		}
 	}
 }
 
 // Configure the MCG Internal Reference Clock
 void setMCGIRClk() {
+	 // Clear clk
+	MCG->C1 &= ~MCG_C1_CLKS_MASK;
+
+	// Set clock source to LIRC
+	// Set IRCLKEN to enable LIRC
+	MCG->C1 |= ((MCG_C1_CLKS(0b01) | MCG_C1_IRCLKEN_MASK));
+
+	// Set IRCS to choose 8 MHz clock
+	MCG->C2 |= MCG_C2_IRCS_MASK;
+
+	// Set FRCDIV for divisor 1
+	MCG->SC &= ~MCG_SC_FCRDIV_MASK;
+	MCG->SC |= MCG_SC_FCRDIV(0b0);
+
+	// Set LIRC_DIV2 for divisor 1
+	MCG->MC &= ~MCG_MC_LIRC_DIV2_MASK;
+	MCG->MC |= MCG_MC_LIRC_DIV2(0b0);
 }
 
 void setTPMClock(){
+	//Choose 8MHz MCGIRCLK
+	setMCGIRClk();
+
+	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
+	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(0b11);
 }
 
 void initPWM() {
+	//turn on clock gating to TPM0
+	SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK;
+
+	//turn on clock gating to PORTD and PORTE
+	SIM->SCGC5 |= (SIM_SCGC5_PORTE_MASK | SIM_SCGC5_PORTD_MASK);
+
+	//configure the RGB LED MUX to be for PWM
+	PORTE->PCR[RED_PIN] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[RED_PIN] |= PORT_PCR_MUX(0b11);
+
+	PORTE->PCR[BLUE_PIN] &= ~PORT_PCR_MUX_MASK;
+	PORTE->PCR[BLUE_PIN] |= PORT_PCR_MUX(0b11);
+
+	PORTD->PCR[GREEN_PIN] &= ~PORT_PCR_MUX_MASK;
+	PORTD->PCR[GREEN_PIN] |= PORT_PCR_MUX(0b100);
+
+	//set pins to output (good practice)
+	GPIOE->PDDR |= ((1 << RED_PIN) | (1 << BLUE_PIN));
+	GPIOD->PDDR |= (1 << GREEN_PIN);
+
+	//the following code is used to setup TPM0
+	//turn off TPM0 by clearing the clock mode
+	TPM0->SC &= ~TPM_SC_CMOD_MASK;
+
+	//clear and set the prescalar 128
+	TPM0->SC &= ~TPM_SC_PS_MASK;
+	TPM0->SC |= (TPM_SC_TOIE_MASK | TPM_SC_PS(0b111));
+
+	//set centre-aligned PWM mode
+	TPM0->SC |= TPM_SC_CPWMS_MASK;
+
+	//initialize count to 0
+	TPM0->CNT = 0;
+
+	//choose and initialize modulo 125
+	TPM0->MOD = 125;
+
+	//Configure TPM0 channels.
+	//IMPORTANT: Configure a REVERSE PWM signal!!!
+	//i.e. it sets when counting up and clears when counting
+	//down. This is because the LEDs are active low.
+	// MS = 10, ELS = 01
+	TPM0->CONTROLS[2].CnSC &= ~(TPM_CnSC_MSB_MASK | TPM_CnSC_MSA_MASK | TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK);
+	TPM0->CONTROLS[2].CnSC |= (TPM_CnSC_MSB(1) | TPM_CnSC_MSA(0) | TPM_CnSC_ELSB(0) | TPM_CnSC_ELSA(1));
+	TPM0->CONTROLS[4].CnSC &= ~(TPM_CnSC_MSB_MASK | TPM_CnSC_MSA_MASK | TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK);
+	TPM0->CONTROLS[4].CnSC |= (TPM_CnSC_MSB(1) | TPM_CnSC_MSA(0) | TPM_CnSC_ELSB(0) | TPM_CnSC_ELSA(1));
+	TPM0->CONTROLS[5].CnSC &= ~(TPM_CnSC_MSB_MASK | TPM_CnSC_MSA_MASK | TPM_CnSC_ELSB_MASK | TPM_CnSC_ELSA_MASK);
+	TPM0->CONTROLS[5].CnSC |= (TPM_CnSC_MSB(1) | TPM_CnSC_MSA(0) | TPM_CnSC_ELSB(0) | TPM_CnSC_ELSA(1));
 }
 
 void startPWM() {
+	//set CMOD for TPM0
+    TPM0->SC |= TPM_SC_CMOD(0b1);
 }
 
 void stopPWM() {
+	//mask CMOD for TPM0
+	TPM0->SC &= ~TPM_SC_CMOD_MASK;
 }
 
 void setPWM(int LED, int percent) {
+	// convert percent into a value
+	int value = (int) (percent / 255.0 * (double) TPM0->MOD);
+	switch(LED) {
+		case(RED):
+		//	set TPM0 control value
+		//	repeat for BLUE and GREEN
+			TPM0->CONTROLS[4].CnV = value;
+			break;
+		case(GREEN):
+			TPM0->CONTROLS[5].CnV = value;
+			break;
+		case(BLUE):
+			TPM0->CONTROLS[2].CnV = value;
+			break;
+		default: printf("invalid LED.\r\n");
+	}
 }
 
 void initButton() {
@@ -128,6 +273,17 @@ void PORTA_IRQHandler() {
 	//configured, but this shows how to do it.
 	if(PORTA->ISFR & (1 << SWITCH_PIN)) {
 		//TODO
+        stopPWM();
+        TPM0->CNT = 0;
+            // TODO: write modulo for 20Hz
+        if (TPM0->MOD == 1563) {
+            // TODO: write modulo for 250Hz
+        	TPM0->MOD = 125;
+        } else {
+            // TODO: write modulo for 20Hz
+        	TPM0->MOD = 1563; // rounded up from 1562.5
+        }
+        startPWM();
 	}
 
     //Write a 1 to clear the ISFR bit.
@@ -156,7 +312,7 @@ int main(void) {
     startPWM();
 
     initADC();
-    startADC(ADC_SE0);
+    startADC(ADC_PIN1_CHANNEL);
     /* Force the counter to be placed into memory. */
     volatile static int i = 0 ;
     /* Enter an infinite loop, just incrementing a counter. */
@@ -178,4 +334,5 @@ int main(void) {
     }
     return 0 ;
 }
+
 
